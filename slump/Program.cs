079821d683a6f586a0fb80a54029a5d437f6ad81
Axelost <axel.osterlund1@gmail.com>
2016-10-28 15:01:16 +0200

    slump

--- slump/Program.cs
+++ slump/Program.cs
@@ -10,6 +10,61 @@ namespace slump
     {
         static void Main(string[] args)
         {
+            /*
+            int sum = 0;
+            int[] tal = new int[10];
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                
+                
+                 tal[i] = rand.Next(1, 21);
+                sum += tal[i];
+                Console.Write(tal[i]+" ");
+
+
+
+            }
+            Console.WriteLine("medelvärdet blir: "+sum/tal.Length);
+            */
+
+            
+            int[] tal = new int[20];
+            Random rand = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+
+
+                tal[i] = rand.Next(15, 31);
+                
+               
+
+
+
+            }
+            
+            
+            Array.Sort(tal);
+            for (int z = 0; z < tal.Length; z++)
+            {
+                Console.Write(tal[z]+" ");
+                if (z==tal.Length/2-1)
+                {
+                    Console.Write(":" +" ");
+                }
+
+            }
+             double min=tal[tal.Length/2-1];
+            double max = tal[tal.Length/2];
+            Console.WriteLine();
+            Console.WriteLine((min + max) / 2);
+            
+            
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
